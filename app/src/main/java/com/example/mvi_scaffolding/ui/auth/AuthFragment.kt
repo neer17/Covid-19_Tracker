acd770a7ac23514020133279a446ce705071a9ff
neer17 <neerajsewanisrt8@gmail.com>
2020-05-04 20:31:13 +0530

    feat: initial code for phone auth added(incomplete).

---
+++ app/src/main/java/com/example/mvi_scaffolding/ui/auth/AuthFragment.kt
@@ -0,0 +1,105 @@
+package com.example.mvi_scaffolding.ui.auth
+
+import android.Manifest
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+
+import com.example.mvi_scaffolding.R
+import com.example.mvi_scaffolding.ui.main.MainActivity
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
+import java.util.concurrent.TimeUnit
+
+
+class AuthFragment : Fragment() {
+    private val TAG = "AppDebug: " + AuthFragment::class.java.simpleName
+
+    private val callbacks by lazy {
+         object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                // This callback will be invoked in two situations:
+                // 1 - Instant verification. In some cases the phone number can be instantly
+                //     verified without needing to send or enter a verification code.
+                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                //     detect the incoming verification SMS and perform verification without
+                //     user action.
+                Log.d(TAG, "onVerificationCompleted:$credential")
+
+                val intent = Intent(activity, MainActivity::class.java)
+                startActivity(intent)
+                activity!!.finish()
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                // This callback is invoked in an invalid request for verification is made,
+                // for instance if the the phone number format is not valid.
+                Log.w(TAG, "onVerificationFailed", e)
+
+                if (e is FirebaseAuthInvalidCredentialsException) {
+                    // Invalid request
+                    // ...
+                } else if (e is FirebaseTooManyRequestsException) {
+                    // The SMS quota for the project has been exceeded
+                    // ...
+                }
+
+                // Show a message and update the UI
+                // ...
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d(TAG, "onCodeSent:$verificationId")
+
+                // Save verification ID and resending token so we can use them later
+//                storedVerificationId = verificationId
+//                resendToken = token
+
+                // ...
+            }
+        }
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_auth, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val verifyBtn = view.findViewById<Button>(R.id.auth_frag_verify_btn)
+        val phoneEt = view.findViewById<EditText>(R.id.auth_frag_mobile_number_et)
+
+        verifyBtn.setOnClickListener {
+            val phoneNumber = phoneEt.text.toString()
+
+            PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNumber, // Phone number to verify
+                60, // Timeout duration
+                TimeUnit.SECONDS, // Unit of timeout
+                activity!!.parent, // Activity (for callback binding)
+                callbacks) // OnVerificationStateChangedCallbacks
+        }
+    }
+}
