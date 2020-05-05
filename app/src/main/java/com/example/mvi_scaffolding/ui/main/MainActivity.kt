1a8c18b0bff5bab8a825e6c768805bfb6f9ce81f
neer17 <neerajsewanisrt8@gmail.com>
2020-05-05 22:28:56 +0530

    feat: toolbar, appconfiguration added.

--- app/src/main/java/com/example/mvi_scaffolding/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/mvi_scaffolding/ui/main/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.mvi_scaffolding.ui.main
 
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.mvi_scaffolding.R
 import dagger.android.support.DaggerAppCompatActivity
 
@@ -9,5 +13,10 @@ class MainActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.main_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
     }
 }
