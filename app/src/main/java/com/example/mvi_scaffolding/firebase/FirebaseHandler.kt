9d05c22dacf7076b379237aa1ab5bd0f862321ae
Shiv Kumar <kumarshivsharma12@gmail.com>
2020-05-11 12:09:42 +0530

    Added util functions for firebase

--- app/src/main/java/com/example/mvi_scaffolding/firebase/FirebaseHandler.kt
+++ app/src/main/java/com/example/mvi_scaffolding/firebase/FirebaseHandler.kt
@@ -8,6 +8,21 @@ import com.google.firebase.messaging.FirebaseMessaging
 
 class FirebaseHandler(context: Context) {
 
+    fun updateUserBasicInfo(uid: String, name: String, phone: Long, isPositive: Boolean) {
+        val ref = Firebase.database.getReference("users")
+            .child(uid)
+        val data = HashMap<String, Any>()
+        data["name"] = name
+        data["phone"] = phone
+        data["isPositive"] = isPositive
+        ref.setValue(data)
+    }
+
+    fun updateCovidPositiveStatus(uid: String, isPositive: Boolean) {
+        Firebase.database.getReference("users")
+            .child(uid).child("isPositive").setValue(isPositive)
+    }
+
     fun updateDataOnNearby(uid: String, receivedUid: String, lat: Double?, lang: Double?) {
         val ref = Firebase.database.getReference("cross_location")
             .child(uid).child(receivedUid)
